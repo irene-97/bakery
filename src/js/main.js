7ee70fa781e3887bdc1e24a0d725806c25cc3970
Irene Artemova <artemova.irene@gmail.com>
2017-12-08 12:10:54 +0400

    Сверстала блок с блюдами

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,17 @@ svg4everybody();
 
 // @include('detect.js')
 // @include('globals.js')
+
+$('.small-slider').slick({
+	infinite: true,
+	slidesToShow: 3,
+	slidesToScroll: 1,
+	asNavFor: '.big-slider',
+});
+
+$('.big-slider').slick({
+	fade: true,
+	arrows: false,
+	asNavFor: '.small-slider',
+	focusOnSelect: true,
+});
